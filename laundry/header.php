53a351f72c04844db383ea15d21bfce11219b0a8
Alif Al Husaini <112994222+alif-09@users.noreply.github.com>
2023-05-22 18:35:14 +0700

    Add files via upload

---
+++ laundry/header.php
@@ -0,0 +1,25 @@
+
+<body>
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+      <a href="" class="logo d-flex align-items-center">
+        <img src="assets/img/laundry.png" alt="">
+        <span>LaundryKu</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="pelanggan.php">Pelanggan</a></li>
+          <li><a class="nav-link scrollto" href="daftar_item.php">Daftar Item</a></li>
+          <li><a class="nav-link scrollto" href="prioritas.php">Prioritas Laundry</a></li>
+          <li><a class="nav-link scrollto" href="permintaan_cucian.php">Permintaan Laundry</a></li>
+          <li><a class="nav-link scrollto" href="#about">Transaksi</a></li>
+          <li><a href="logout.php">Log Out</a></li> <!-- Mengarahkan ke halaman logout.php -->
+        </ul>
+      </nav>
+    </div>
+  </header><!-- End Header -->
+</body>
+
+</html>
